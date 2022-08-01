b2670ab66fc245171fc8676ca8217ccb29396c2d
unknown <taphu0812@gmail.com>
2022-08-01 14:57:53 +0700

    upload

---
+++ src/Point3D.java
@@ -0,0 +1,41 @@
+public class Point3D extends Point2D {
+    private float z;
+
+    public Point3D() {
+
+    }
+
+    public Point3D(float z){
+        this.z = z;
+    }
+    public Point3D(float x, float y, float z){
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public void setXYZ(float x, float y, float z){
+        super.setXY(x,y);
+        this.z = z;
+    }
+    public float[] getXYZ(){
+        float[] xyz = new float[3];
+        return xyz;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                " x= " + super.getX()
+                 + " y= " + super.getY()+
+                " z= " + z
+                +
+                '}';
+    }
+}
