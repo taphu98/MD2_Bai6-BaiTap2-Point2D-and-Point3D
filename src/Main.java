fe068c4956f9f1f3905c968e7d5f32934b723b0d
unknown <taphu0812@gmail.com>
2022-08-01 15:08:49 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Point2D point2D = new Point2D(5,7);
         System.out.println(point2D);
-        Point3D point3D = new Point3D(3,5,7);
+        Point3D point3D = new Point3D(point2D.getX(), point2D.getY(), 7);
         System.out.println(point3D);
+        System.out.println(Arrays.toString(point3D.getXYZ()));
     }
 }
